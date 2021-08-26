3fcf3941cc4341e3a0e29c7155e8b5cd86f5e20a
Jhonatan Bezerra <pixeel44@gmail.com>
2021-08-26 12:55:35 -0300

    Opa

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const server = express();
 const animals = require('./src/data/animals.json');
+const animal = require('./src/data/animal.json');
 
 server.get('/animals', (req,res) => {
     return res.json(animals)
+});
+
+server.get('/animal', (req,res) => {
+    return res.json(animal)
 });
 
 server.listen(3000, () => {
